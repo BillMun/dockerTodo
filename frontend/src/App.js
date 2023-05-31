34a18c051098358771dfe23429d94a5c5fb183ad
Bill Munkacsy <billmunkacsy@gmail.com>
2023-05-31 10:57:33 -0400

    adding frontend to project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import axios from "axios"
+
+
+export default function App (props){
+    return(
+        <div>
+            Welcome to my baby tracking app!
+        </div>
+    )
+}
